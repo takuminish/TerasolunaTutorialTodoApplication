c14cfe40538025a11bffaf86acd11912194e24c5
takuminish <s15t253@stu.kagawa-u.ac.jp>
2020-01-12 22:18:29 +0900

    11.1.4.2.1. Controllerの作成

---
+++ src/main/java/com/example/todo/app/todo/TodoController.java
@@ -0,0 +1,10 @@
+package com.example.todo.app.todo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("todo")
+public class TodoController {
+
+}
